971dbcb2f7ee3c5284eb0cd4a97edbb9047b799f
Durjoy-dcp <unknownccxv@gmail.com>
2022-12-29 02:12:28 +0600

    complete marking

--- index.js
+++ index.js
@@ -16,7 +16,50 @@ app.use(express.json());
 
 const username = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
+const uri = `mongodb+srv://${username}:${password}@cluster0.lgdlxzl.mongodb.net/?retryWrites=true&w=majority`;
 
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    const database = client.db("victor-task-manager");
+    const tasks = database.collection("task");
+    app.post("/insert", async (req, res) => {
+      const task = req.body;
+      const result = await tasks.insertOne(task);
+      console.log(result);
+      res.send(result);
+    });
+    app.get("/mytasks", async (req, res) => {
+      const email = req.query.email;
+      const query = {
+        // user: email
+        isComplete: false,
+      };
+      const result = await tasks.find(query).toArray();
+      res.send(result);
+    });
+    app.patch("/completed", async (req, res) => {
+      const id = req.query.id;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          isComplete: true,
+        },
+      };
+      const result = await tasks.updateOne(filter, updateDoc);
+      res.send(result);
+      console.log(result);
+      console.log(id);
+    });
+  } finally {
+  }
+}
+
+run().catch((err) => console.log(err));
 app.get("/", (req, res) => {
   res.send("Hello World!This is ");
 });
